2b84acee67e4147aa261f279939255abfb172ad8
getHecked <arjn.nair@gmail.com>
2017-10-18 16:15:35 +0200

    More with sets/lists

--- comprehension.py
+++ comprehension.py
@@ -2,5 +2,31 @@
 
 participants = ['Alex', 'Bob', 'Claudia', 'Bob', 'Daniel', 'Alex', 'Alex']
 
-s = set(participants)
+s = sorted(list(set(participants)))
 s
+
+for i in range(len(s)):
+    print(f"{s[i]}", end=', ')
+    if (i==len(s)-1):
+        print(f'and {s[i]}', end='.')
+
+#enumerate(<list>) --> returns a pair of data (index,value)
+
+#new app
+email = {
+    'Leo': 'leo@gmail.com',
+    'Catalina': 'catalin@gmail.com',
+    'Ben': 'benedikt.corkill@is-ostrava.cz'
+}
+
+#prints the pair
+for name in email.items():
+    print(name)
+
+#prints values
+for name in email.values():
+    print(name)
+
+#prints keys
+for name in email.keys():
+    print(name)
